dc330f21deaf25b25d3a9008ea0fd83bd49367e9
masis_dk <masis.professionnel@gmail.com>
2025-02-23 13:51:18 +0100

    fix: font-family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import "@/styles/globals.css";
 import '@mantine/core/styles.css';
 import type { AppProps } from "next/app";
 import {Layout} from "@/components/Layout/Layout";
-import {MantineProvider} from "@mantine/core";
+import {createTheme, MantineProvider} from "@mantine/core";
 import {PrismicPreview} from "@prismicio/next";
 import {repositoryName} from "@/prismicio";
 
+const theme = createTheme({
+  fontFamily: 'Libre Franklin, sans-serif',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <Layout {...pageProps}>
         <Component {...pageProps} />
         <PrismicPreview repositoryName={ repositoryName }/>
