7b8cc1eb861df1b0d6b4ef2ad11801e8445f3622
CedricSalaun <salaun.cedric@gmail.com>
2025-02-23 18:13:39 +0100

    contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,12 +1,31 @@
-import {GetStaticPropsContext} from "next";
+import {GetStaticPropsContext, InferGetStaticPropsType} from "next";
 import {createClient} from "@/prismicio";
+import {Box, Container, Space, Title} from "@mantine/core";
+import {PrismicRichText} from "@prismicio/react";
+import React from "react";
+import {contactpageCVA} from "@/styles/page.styles";
 
-export default function Contact({ nav }: any) {
-  console.log(nav)
+export default function Contact({page}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const {titre: title, content} = page.data
+  console.log({content})
+
   return (
-    <div>
-      <h1>contact</h1>
-    </div>
+    <Container id='Contact-section' fluid className={contactpageCVA.root()}>
+      <Box className={contactpageCVA.title()}>
+        <Title>{title}</Title>
+      </Box>
+      <Space h='xl'/>
+      <Box className={contactpageCVA.info()}>
+        {content.map(({info}, index: number) => (
+          <>
+            <PrismicRichText key={index} field={info}/>
+            <Space h='xl'/>
+          </>
+        ))}
+      </Box>
+      <Space h='xl'/>
+      <Space h='xl'/>
+    </Container>
   )
 }
 
@@ -14,11 +33,14 @@ export async function getStaticProps({ previewData }: GetStaticPropsContext) {
   const client = createClient({ previewData })
   const document = await client.getSingle("menu");
   const footer = await client.getSingle("footer");
+  const page = await client.getSingle('contact')
+
 
   return {
     props: {
       nav: document,
       footer,
+      page
     },
   };
 }
--- src/styles/page.styles.ts
+++ src/styles/page.styles.ts
@@ -3,3 +3,9 @@ import {cva} from "class-variance-authority";
 export const homepageCVA = {
   root: cva(['!px-(--spacing-md) lg:px-[0] max-w-[1280px] mx-auto'])
 }
+
+export const contactpageCVA = {
+  root: cva(['w-full m-auto !max-w-(--spacing-container) !px-(--spacing-md) lg:px-[0] max-w-[1280px] mx-auto']),
+  title: cva(['mb-16']),
+  info: cva(['text-[18px]']),
+}
