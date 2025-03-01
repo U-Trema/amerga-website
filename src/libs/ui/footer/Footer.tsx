de5d970bd15fe9d5833e69a5ec2119411a709e84
masis_dk <masis.professionnel@gmail.com>
2025-03-01 21:20:16 +0100

    fix: footer

--- src/libs/ui/footer/Footer.tsx
+++ src/libs/ui/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, ReactNode} from 'react';
 import {PrismicRichText} from '@prismicio/react';
 import {PrismicNextImage, PrismicNextLink} from '@prismicio/next';
 import {Container, Flex, Grid, GridCol} from '@mantine/core';
@@ -10,6 +10,10 @@ import {
   footerGridCVA
 } from '@/libs/ui/footer/footer.classes';
 
+const slogan = {
+  paragraph: ({ children }: { children: ReactNode }) => <p className='[&>strong]:font-semibold'>{children}</p>,
+}
+
 export const Footer: FC<any> = ({footer}) => {
   if (!footer) return null;
 
@@ -20,25 +24,24 @@ export const Footer: FC<any> = ({footer}) => {
           columns={12}
           gutter={6}
           justify='left'
+          className='mb-64 lg:mb-128'
         >
-          <GridCol span={5} className={footerGridCVA.firstCol()}>
+          <GridCol span={{ base: 12, md: 12, lg: 6 }} className={footerGridCVA.firstCol()}>
             <section>
               <Flex direction='column' gap='xs' className={footerGridCVA.firstColBox()}>
                 <PrismicNextImage field={footer.data.footer_logo} alt="" />
-                <PrismicRichText field={footer.data.footer_slogan}/>
+                <PrismicRichText field={footer.data.footer_slogan} components={slogan}/>
               </Flex>
             </section>
           </GridCol>
-          <GridCol
-            span={{base: 12, md: 6}}
-            offset={{base: 0, md: 2}}
-          >
+
+          <GridCol span={{base: 12, md: 12, lg: 6}} offset={{base: 0, md: 0}}>
             <section>
               <Flex className={footerGridCVA.col()}>
                 {footer.data.columns.map((item: any, index: number) => (
-                  <Flex key={index} direction='column'>
+                  <Flex key={index} direction='column' className='basis-1/2 md:basis-1/3'>
                     <p className={footerColumnCVA.title()}>{item.title}</p>
-                    <ul>
+                    <ul className='flex flex-col gap-[6px] sm:gap-8'>
                       {item.link.map((link: any) => (
                         <li key={link.key} className={footerColumnCVA.link()}>
                           <PrismicNextLink field={link}/>
@@ -51,9 +54,10 @@ export const Footer: FC<any> = ({footer}) => {
             </section>
           </GridCol>
         </Grid>
+
         <section>
           <Flex direction='column'>
-            <Flex direction='row' gap={8} className={footerBottomCVA.first()}>
+            <Flex direction='row' gap={16} className={footerBottomCVA.first()}>
               {footer.data.slices.map((slice: any) =>
                 slice.primary.section_links.map((item: any, index: number) => (
                   <PrismicNextLink key={index} field={item.section_link_url}>
@@ -66,7 +70,7 @@ export const Footer: FC<any> = ({footer}) => {
               <span className={footerBottomCVA.copyright()}>{footer.data.footer_copyright}</span>
               <Flex className={footerBottomCVA.legal()}>
                 {footer.data.legal_link.map((link: any, index: number) => (
-                  <PrismicNextLink key={index} field={link}/>
+                  <PrismicNextLink key={index} field={link} className='font-semibold text-[10px]'/>
                 ))}
               </Flex>
             </Flex>
