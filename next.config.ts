409b89cca739024e5d0c7cdf810107d0b3e9dfed
masis_dk <masis.professionnel@gmail.com>
2025-03-04 08:58:20 +0100

    add headers

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,20 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   trailingSlash: true,
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ];
+  }
 };
 
 export default nextConfig;
